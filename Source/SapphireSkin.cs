321c3fd5de742f40dec3c351491f3c76b74eed29
praniclift <praniclift@gmail.com>
2015-04-08 21:17:43 -0400

    Skin started

---
+++ Source/SapphireSkin.cs
@@ -0,0 +1,24 @@
+using ICities;
+
+namespace SapphireSkin
+{
+
+    public class Mod : IUserMod
+    {
+
+        public string Name
+        {
+            get
+            {
+                return "Simple and Clean Skin";
+            }
+        }
+
+        public string Description
+        {
+            get { return "Lorem ipsum dolor en mi cabesa"; }
+        }
+
+    }
+
+}
